537d88fdee2dbb3f27dd73d5268c6d0d5c956d16
Anakael <shemeldima2@yandex.ru>
2021-11-11 14:29:07 +0300

    Update keymap.lua

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -22,6 +22,7 @@ map('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', default_opts)
 map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', default_opts)
 map('v', '<space>a', ":'<,'>lua vim.lsp.buf.range_code_action()<CR>", default_opts)
 map('n', '[g', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', default_opts)
+map('n', ']g', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', default_opts)
 map('', '<leader>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', default_opts)
 
 -- telescope
