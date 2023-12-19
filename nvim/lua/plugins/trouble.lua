d0d5b10d2a236689239f31d9a7dc92db622ae654
dmitry <shemeldima2@yandex.ru>
2023-12-19 23:01:06 +0300

    add snip

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,11 +1,11 @@
 local setup = function()
-    local trouble = require("trouble")
+    local trouble = require('trouble')
     trouble.setup({
         height = 30,
         severity = vim.diagnostic.severity.ERROR
     })
     local map = vim.keymap.set
-    map('n', '<leader>xt', function() trouble.toggle("workspace_diagnostics") end)
+    map('n', '<leader>xt', function() trouble.toggle('workspace_diagnostics') end)
 end
 
 local M = {
